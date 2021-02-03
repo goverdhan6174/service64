8bfb8882d6731bfe5f9aadfa451b0d121ffd9e16
Goverdhan Singh <goverdhansingh669@gmail.com>
2021-02-03 23:08:54 +0530

    added react-snap

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import Store from './store';
+import React from "react";
+import { hydrate, render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import Store from "./store";
 
-
-ReactDOM.render(
-  <Provider store={Store} >
+const SITE = (
+  <Provider store={Store}>
     <Router>
       <App />
     </Router>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
-
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(SITE, rootElement);
+} else {
+  render(SITE, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
